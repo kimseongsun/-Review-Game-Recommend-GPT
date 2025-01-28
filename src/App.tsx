b5065622b79352fdef005760d1976e9ac8ef8368
kimseongsun <kimseongson@gmail.com>
2025-01-29 02:02:30 +0900

    LoginPage demo, Mainpage demo using router

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import MainPage from "./Mainpage";
+import LoginPage from "./LoginPage";
 
-function App() {
-  const google_logo =
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png";
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Game Recommend GPT</h1>
-      <p className="read-the-docs">
-        This service will recommend you a game based on your preferences.
-        <br></br>Feel free to explore and find your next favorite game!
-      </p>
-      <div className="card">
-        <button>
-          <img src={google_logo} className="logo" alt="Vite logo" />
-          <br></br>
-          Login With Google
-        </button>
-      </div>
-    </>
+    <div>
+      <Routes>
+        <Route path="" element={<MainPage />} />
+        <Route path="login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
--- src/Mainpage.tsx
+++ src/Mainpage.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function MainPage() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h5>Game Recommend GPT</h5>
+        <textarea placeholder="Type here..." rows={5} cols={40}></textarea>
+        <button>OK</button>
+      </div>
+    </>
+  );
 }
-
 export default MainPage;
